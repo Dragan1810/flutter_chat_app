6f34a15a12aa897a8383ee24d190a71532068331
Dragan <dragan.novakovic@bridgewaterlabs.com>
2019-06-26 20:56:35 +0200

    init

---
+++ backend/target/rls/debug/deps/backend-93b87bafa193a695.d
@@ -0,0 +1,5 @@
+/Users/dragan1810/Desktop/flutter_chat_app/backend/target/rls/debug/deps/backend-93b87bafa193a695.rmeta: src/main.rs
+
+/Users/dragan1810/Desktop/flutter_chat_app/backend/target/rls/debug/deps/backend-93b87bafa193a695.d: src/main.rs
+
+src/main.rs:
---
+++ backend/target/rls/debug/deps/backend-dc1984bc78119893.d
@@ -0,0 +1,5 @@
+/Users/dragan1810/Desktop/flutter_chat_app/backend/target/rls/debug/deps/backend-dc1984bc78119893.rmeta: src/main.rs
+
+/Users/dragan1810/Desktop/flutter_chat_app/backend/target/rls/debug/deps/backend-dc1984bc78119893.d: src/main.rs
+
+src/main.rs:
